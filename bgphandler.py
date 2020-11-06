674f834777b1fbed403bc128ec7091816f8286d9
zbs <jackmygreat@gmail.com>
2020-11-06 19:55:20 +0800

    redis

--- bgphandler.py
+++ bgphandler.py
@@ -1,11 +1,10 @@
 import pybgpstream
 import redis
-import asyncio
 
 # init bgpStream
 bgpStream = pybgpstream.BGPStream(record_type='ribs')
 bgpStream.stream.set_data_interface('singlefile')
-bgpStream.stream.set_data_interface_option('singlefile', 'rib-file', 'rib.20201031.2000.bz2')
+bgpStream.stream.set_data_interface_option('singlefile', 'rib-file', 'rib.20201002.0200.bz2')
 bgpStream.stream.set_data_interface_option('singlefile', 'rib-type', 'mrt')
 bgpStream.stream.add_interval_filter(0, 0)
 
@@ -30,19 +29,20 @@ node_prefix = 'node_asn_{:s}'
 #       pipe.sadd(node_prefix.format(ases[i-1]),val)
 #    pipe.execute()
 
-async def handle(elem):
+def handle(elem):
     ases = elem.fields["as-path"].split(" ")
     if len(ases) == 1:
         return
     print(ases)
     pipe = rp.pipeline()
     pipe.sadd(nodeAll, *ases)
-    for i, val in enumerate(list, 1):
-        pipe.sadd(node_prefix.format(ases[i - 1]), val)
-    await pipe.execute()
+    for i in range(0,len(ases)-1):
+        print(i)
+        pipe.sadd(node_prefix.format(ases[i]), ases[i+1])
+    pipe.execute()
     return
 
 
 for elem in bgpStream:
     print(elem)
-    asyncio.run(handle(elem))
+    handle(elem)
